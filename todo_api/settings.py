8ced62f85e363d3d6881b51469efe94f286c1916
phatspida <81350755+phatspida@users.noreply.github.com>
2022-07-13 22:18:43 +0100

    Update settings.py
    Edited Safely

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)=76dub+bbd%kq=)5b$bv1j!#m4e+i&6o0zjut@a1uacq0$8#_'
+SECRET_KEY = '#############'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
